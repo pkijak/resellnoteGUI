c1ae3b9acf39ca522fe7cf61d21f43aa8873a1e8
pkijak <46262684+pkijak@users.noreply.github.com>
2019-02-13 12:05:10 +0100

    test

---
+++ src/main/java/com/kijak/resellnote/App.java
@@ -0,0 +1,13 @@
+package com.kijak.resellnote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
